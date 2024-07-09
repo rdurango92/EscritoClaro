62f5f30d3e27bfa268d061e5331444a1db4c491b
Ruben Durango <rdurango92@gmail.com>
2024-07-09 16:35:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ import torch
 
 #%% Cargar modelo y procesador
 
-ocr_model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
-ocr_processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
+ocr_model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large", trust_remote_code=True)
+ocr_processor = AutoProcessor.from_pretrained("microsoft/Florence-2-large", trust_remote_code=True)
 
 # Cargar modelo para corrección de texto
 device = 0 if torch.cuda.is_available() else -1 # -1 para CPU, 0 para GPU
-correction_model = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B", device=device)
+#correction_model = pipeline("text2text-generation", model="vennify/t5-base-grammar-correction", device=device)
 
 #%% Carga de Imagen
 
-image_path = "data/page1.jpg"
+image_path = "data/page3.jpg"
 image = Image.open(image_path)
 
 #%% Funcion para correr el ejemplo
@@ -41,20 +41,9 @@ def run_ocr(task_prompt, image):
     parsed_answer = ocr_processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
     return parsed_answer['<OCR>']
 
-def correct_text(text):
-    prompt = f"Corrige el siguiente texto en español:\n\n{text}\n\nTexto corregido:"
-    corrected_text = correction_model(prompt, max_length=1024, num_return_sequences=1)[0]['generated_text']
-    # Eliminar el prompt inicial del texto generado
-    corrected_text = corrected_text.split("Texto corregido:")[1].strip()
-    return corrected_text
-
 #%% Ejecucion de la tarea
 prompt = "<OCR>"
 ocr_text = run_ocr(prompt, image)
 print("Texto OCR:", ocr_text)
 
 #%%
-corrected_text = correct_text(ocr_text)
-print("Texto corregido:", corrected_text)
-
-# %%
